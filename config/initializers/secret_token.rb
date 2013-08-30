678faee603f040436166fce973f406ad525f7093
Roy van de Water <dev+roy@integrumtech.com>
2013-08-30 09:18:55 -0700

    Add rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Retrotactular::Application.config.secret_key_base = '9cd92ebb9bb7ed8bf94367b2517e489623f1444d40c66489b3fb1819dbbcd37a74fdea3b4991d7da5dce546337a32db60dd1c751b181aca0612db235e99b0fe0'
